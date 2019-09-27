83f06a2c035b165055be7f8f787031c9b402e010
Christian Römer <croemer@informationsfabrik.de>
2019-09-27 16:20:26 +0200

    Add update mechanism through JSON file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-import subprocess
+from typing import List
+
 import os
-import re
-from pathlib import Path
+import json
+import time
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -15,10 +17,52 @@ def read_root():
         "message": "Congratulations, you have reached the green wave backend API, Muenster's awesome new quality of life enhancement for the cities bikers. You can find this API's documentation at http://<server address>/docs"
     }
 
+
+@app.get("/update_phase")
+def update_phase(is_green: bool, phase_length: float):
+    data = {
+        "is_green": is_green,
+        "phase_length": phase_length,
+        "commit_time": time.time()
+    }
+    with open("data.json", "w") as data_file:
+        json.dump(data, data_file)
+
+
 @app.get("/seconds_phase_left")
 def get_seconds_phase_left():
+    if os.path.isfile("data.json"):
+        with open("data.json", "r") as data_file:
+            data_dict = json.load(data_file)
+            return {
+                "status": "OK",
+                "message": "",
+                "id_light": 1,
+                "is_green": bool(data_dict["is_green"]),
+                "is_red": not bool(data_dict["is_green"]),
+                "seconds_phase_left": data_dict["commit_time"] - time.time(),
+                "seconds_phase_total": data_dict["phase_length"],
+            }
+
+    else:
+        # no data has been written yet
+        return {
+            "status": "ERROR",
+            "message": "No data history",
+            "id_light": 1,
+            "is_green": False,
+            "is_red": False,
+            "seconds_phase_left": 40.000,
+            "seconds_phase_total": 40.000,
+        }
+
+
+@app.get("/seconds_phase_left_mockup_static")
+def seconds_phase_left_mockup_static():
     # mockup
     return {
+        "status": "OK",
+        "message": "",
         "id_light": 1,
         "is_green": True,
         "is_red": False,
