a9884720eec1d12bc0439ab289f120762f3d7bc0
Christian Römer <croemer@informationsfabrik.de>
2019-09-27 14:18:46 +0200

    Add mockup code

--- app/main.py
+++ app/main.py
@@ -15,3 +15,12 @@ def read_root():
         "message": "Congratulations, you have reached the green wave backend API, Muenster's awesome new quality of life enhancement for the cities bikers. You can find this API's documentation at http://<server address>/docs"
     }
 
+@app.get("/seconds_phase_left")
+def get_seconds_phase_left():
+    # mockup
+    return {
+        "id_light": 1,
+        "is_green": True,
+        "is_red": False,
+        "seconds_phase_left": 35.123
+    }
